1c7ab2fcc6facdfea81830422cb8cc2ce79be183
Pranav Dronavalli <dronavallipranav@gmail.com>
2023-08-15 22:40:21 -0500

    fixed styling bug, added picture

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
     autoprefixer: {},
     ...(production && {
       '@fullhuman/postcss-purgecss': {
-        content: ['./src/**/*.astro', './src/**/*.html'],
+        content: ['./src/**/*.astro', './src/**/*.html', "./src/**/posts/*.md", "./src/**/posts/*.html", "./src/**/posts/*.astro", "./src/*.astro"],
         defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
       },
     }),

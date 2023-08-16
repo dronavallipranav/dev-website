5adfd126e33ad7ab8b468c4f871f11ded4c51890
Pranav Dronavalli <dronavallipranav@gmail.com>
2023-08-16 05:48:02 -0500

    fixed build process breaking styling

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,11 +4,11 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(production && {
-      '@fullhuman/postcss-purgecss': {
-        content: ['./src/**/*.astro', './src/**/*.html', "./src/**/posts/*.md", "./src/**/posts/*.html", "./src/**/posts/*.astro", "./src/*.astro"],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      },
-    }),
+    //...(production && {
+    //'@fullhuman/postcss-purgecss': {
+        //content: ['./src/**/*.astro', './src/**/*.html', "./src/**/posts/*.md", "./src/**/posts/*.html", "./src/**/posts/*.astro", "./src/*.astro"],
+        //defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      //},
+    //}),
   },
 };

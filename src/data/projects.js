3d7e5892bf8f8d906d9e78d6fb0f5dc2a25c6ccd
Pranav Dronavalli <dronavallipranav@gmail.com>
2023-12-31 13:46:26 -0600

    generate projects programatically

---
+++ src/data/projects.js
@@ -0,0 +1,25 @@
+let projects = [
+  {
+    title: "Rust Obfuscator - ",
+    status: "IN PROGRESS",
+    description:
+      "creating a open source obfuscator for Rust, currently have variable renaming, but working on string and integer literal encryption",
+    image: "/rustacean.jpg",
+  },
+  {
+    title: "GameBoy Emulator - ",
+    description: "Take a look at the current state of my Emulator",
+    status: "IN PROGRESS",
+    image: "/gameboy.jpeg",
+    link: "https://github.com/dronavallipranav/GBEmulator",
+  },
+  {
+    title: "MadHacks Website",
+    status: "",
+    description: "Our website currently in place for MadHacks",
+    image: "/madhacks2.jpeg",
+    link: "https://www.madhacks.io/",
+  },
+];
+
+export default projects;

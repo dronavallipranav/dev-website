2eae8449a26dce1414becd31ee0323b8ab3eb182
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-10-21 21:09:49 -0500

    add live download update to rust proj

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,26 @@
+const fetchCrateDownloads = async (crateName) => {
+  const response = await fetch(`https://crates.io/api/v1/crates/${crateName}`, {
+    headers: {
+      "User-Agent": "Pranav Dronavalli's Website (dronavallipranav@gmail.com)",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error fetching crate data: ${response.statusText}`);
+  }
+  const data = await response.json();
+  return data.crate.downloads;
+};
+
 let projects = [
   {
-    title: "Swapper",
+    title: "Rust Obfuscator",
     status: "",
     description:
-      "platform for item exchange and reducing waste by connecting people with items they need -- 1st place at DeveloperWeek 2024 (2k+ particpants)",
-    image: "/swap.png",
-    link: "https://devpost.com/software/swapper-8u4rq0",
+      "open source automatic rust obfuscator and procedural macro library for fine-grained control",
+    image: "/rustacean.jpg",
+    link: "https://github.com/dronavallipranav/rust-obfuscator",
+    liveData: async () => await fetchCrateDownloads("cryptify"),
   },
   {
     title: "Low Elo Chess",
@@ -16,12 +31,12 @@ let projects = [
     link: "https://github.com/dronavallipranav/dumb-chess",
   },
   {
-    title: "Rust Obfuscator",
+    title: "Swapper",
     status: "",
     description:
-      "open source automatic rust obfuscator and procedural macro library for fine-grained control",
-    image: "/rustacean.jpg",
-    link: "https://github.com/dronavallipranav/rust-obfuscator",
+      "platform for item exchange and reducing waste by connecting people with items they need -- 1st place at DeveloperWeek 2024 (2k+ particpants)",
+    image: "/swap.png",
+    link: "https://devpost.com/software/swapper-8u4rq0",
   },
   {
     title: "GameBoy Emulator - ",

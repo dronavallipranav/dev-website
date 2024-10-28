8114adbfefc9ecbf4ae31fff8410bcf86b11eb7e
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-10-28 12:51:59 -0500

    use lambda for cors

--- public/scripts/fetchDownloads.js
+++ public/scripts/fetchDownloads.js
@@ -1,11 +1,9 @@
 async function fetchDownloads() {
   try {
     console.log("Fetching download count...");
-    const response = await fetch("https://crates.io/api/v1/crates/cryptify", {
-      headers: {
-        "User-Agent": "Pranav Dronavalli Website (dronavallipranav@gmail.com)",
-      },
-    });
+    const response = await fetch(
+      "https://jtd1lexbk4.execute-api.us-east-2.amazonaws.com/default/fetchDownloads"
+    );
 
     if (!response.ok) {
       throw new Error(`Error fetching data: ${response.statusText}`);

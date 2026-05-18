064117f85ff44c3b3202d68ffc908d3c7603a3fe
Pranav Dronavalli <dronavallipranav@gmail.com>
2026-05-17 22:23:22 -0500

    update dark mode colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        customGray: '#121212',
-        lighterGray: '#1F1B24',
-        titlePurple: '#BB86FC',
-        titlePurpleDark: '#A675E2',
-        containerGray: '#1F1A24',
-        byz: "#AA336A",
+        customGray: '#0f0f0f',
+        lighterGray: '#171717',
+        titlePurple: '#f5f5f5',
+        titlePurpleDark: '#d4d4d4',
+        containerGray: '#1a1a1a',
+        byz: "#262626",
         boxShadow: {
-          'purple-lg': '0 10px 20px -5px rgba(187, 134, 252, 0.8), 0 6px 10px -2px rgba(187, 134, 252, 0.6)',
+          'purple-lg': '0 10px 20px -5px rgba(0, 0, 0, 0.6), 0 6px 10px -2px rgba(0, 0, 0, 0.4)',
         },
       },
     },
